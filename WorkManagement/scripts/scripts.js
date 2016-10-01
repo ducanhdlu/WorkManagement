3ec002a8a80f98acb7309c223997cff3161f1e57
DoAnTDA <phamducanhk36@gmail.com>
2016-10-01 12:06:42 +0700

    Created Project

---
+++ WorkManagement/scripts/scripts.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    var clickcount = 0;
+    $('.toggle-btn').click(function () {
+        $('#navigation-bar').toggleClass('hidden');
+        if (clickcount % 2 == 0) {
+            $('#content').css('width', '100%');
+        } else {
+            $('#content').css('width', '80%');
+        }
+        clickcount++;
+    });
+});
