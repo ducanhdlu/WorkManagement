b390c64f09524113360ecfbbe24301955af35416
DoAnTDA <phamducanhk36@gmail.com>
2016-10-12 09:57:48 +0700

    manager
    Manager

---
+++ WorkManagement/Controllers/TimekeepingController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WorkManagement.Models;
+using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc;
+
+namespace WorkManagement.Controllers
+{
+    public class TimekeepingController : Controller
+    {
+        private QLNghiPhepEntities1 db = new QLNghiPhepEntities1();
+        // GET: Timekeeping
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Check
+        public ActionResult Check()
+        {
+            return View();
+        }
+
+        // GET: ManagerToday
+        public ActionResult ManagerToday()
+        {
+            var list = db.Timekeepings;
+            return View(list.ToList());
+        }
+
+        // GET: ManagerOption
+        public ActionResult ManagerOption()
+        {
+            
+            return View();
+        }
+    }
+}
